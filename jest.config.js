61c98718fb921984d0b6635e7a0feaebc4fc9725
Anuroop <anuroopjoy@gmail.com>
2021-11-03 14:19:52 +0530

    init

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+require("jest-preset-angular/ngcc-jest-processor");
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  moduleNameMapper: {
+      "lodash-es": "lodash"
+  },
+};
